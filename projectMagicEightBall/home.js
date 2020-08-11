ff759d99a2d2c138dad2c645fa5e0fc062c88388
Lê Nguyễn Thiện Nhân <ThienNhan@MacBook-Air-cua-Le.local>
2020-08-11 21:21:34 +0700

    JS

--- projectMagicEightBall/home.js
+++ projectMagicEightBall/home.js
@@ -1,11 +1,10 @@
 //define a variable called userName that is set to an empty string.
-let userName = 'Nhan';
+let userName = prompt('what is your name ?');
 
 //create a ternary expression that decides what to do if the user enters a name or not.
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
-
 //Create a variable named userQuestion
-let userQuestion = 'What is your name?';
+let userQuestion = prompt('Any question?');
 
 //Write a console.log() for the userQuestion, stating what was asked
 console.log(`The user asked: ${userQuestion}`);
@@ -14,35 +13,33 @@ console.log(`The user asked: ${userQuestion}`);
 let randomNumber = Math.floor(Math.random() * 8);
 
 //Create one more variable named eightBall
-let eightBall = randomNumber;
+let eightBall = '';
 
 //Think about utilizing if/else or switch statements.
-switch (eightBall) {
+switch (randomNumber) {
   case 0 :
   eightBall = 'It is certain'
   break;
-  case 2 :
+  case 1 :
   eightBall = 'It is decidedly so'
   break;
-  case 3 :
+  case 2 :
   eightBall = 'Reply hazy try again'
   break;
-  case 4 :
+  case 3 :
   eightBall = 'Cannot predict now'
   break;
-  case 5 :
+  case 4 :
   eightBall = 'Do not count on it'
   break;
-  case 6 :
+  case 5 :
   eightBall = 'My sources say no'
   break;
-  case 7 :
+  case 6 :
   eightBall = 'Outlook not so good'
   break;
-  case 8 :
+  case 7 :
   eightBall = 'Signs point to yes'
-  break;
-  case 1 :
   break;
 }
 console.log(`The eight ball answered: ${eightBall}`);
