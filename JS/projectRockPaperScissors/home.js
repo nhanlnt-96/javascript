8c87fe3f49439bf4efa0b5c150de3f3ca35e22db
Lê Nguyễn Thiện Nhân <ThienNhan@MacBook-Air-cua-Le.local>
2020-09-02 11:23:03 +0700

    JS

--- JS/projectRockPaperScissors/home.js
+++ JS/projectRockPaperScissors/home.js
@@ -1,3 +1,60 @@
-const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-  };
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('error');
+  }
+};
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+};
+
+function determineWinner(userChoice,computerChoice) {
+  if (userChoice === computerChoice) {
+    return 'Tie!';
+  } 
+  
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'Computer won!';
+    } else {
+      return 'User won';
+    }
+  }
+  
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'Computer won!';
+    } else {
+      return 'User won!';
+    }
+  }
+  
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'Computer won!';
+    } else {
+      return 'User won!';
+    }
+  }
+};
+
+function playGame() {
+  const userChoice = getUserChoice('paper');
+  const computerChoice = getComputerChoice();
+  console.log(`You threw: ${userChoice}`);
+  console.log(`Computer threw: ${computerChoice}`);
+  console.log(determineWinner(userChoice,computerChoice));
+};
+
+console.log(playGame());
