b259fea87e2fd1edc52fea35ee5549ded194af4b
Lê Nguyễn Thiện Nhân <ThienNhan@MacBook-Air-cua-Le.local>
2020-09-06 12:51:05 +0700

    .

---
+++ JS/projectAnimation/home.js
@@ -0,0 +1,24 @@
+var sliderPosition = 0;
+
+State
+
+props
+
+function handleSliderMoving(movingType) {
+    const movingContainer = document.getElementById('moving-container');
+    const wrapperWidth = movingContainer.offsetWidth;
+    
+    if (movingType === 'left') {
+        sliderPosition += wrapperWidth;
+        if (sliderPosition > 0) {
+            sliderPosition = -2160;
+        }
+    } else {
+        sliderPosition -= wrapperWidth;
+        if (sliderPosition < -2160) {
+            sliderPosition = 0;
+        }
+    }
+
+    movingContainer.style.left = `${sliderPosition}px`;
+}
