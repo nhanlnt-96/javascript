7c41b597473f8de9252dc87d7b4cad0bd1c6e452
Lê Nguyễn Thiện Nhân <ThienNhan@MacBook-Air-cua-Le.local>
2020-09-23 14:14:18 +0700

    .

--- REACTJS/react-homework/form-weather/src/App.js
+++ REACTJS/react-homework/form-weather/src/App.js
@@ -21,15 +21,23 @@ class App extends React.Component {
                 <img src={logo} alt="logo" />
               </div>
 
-              <div className="info-form">
-                <div className="input-info">
-                  <label for="address">Address</label>
-                  <input name="address" type="text" />
+              <div className="content-form">
+                <div className="info-form">
+                  <div className="input-info">
+                    <label for="address">Address</label>
+                    <input name="address" type="text" />
+                  </div>
+
+                  <div className="submit-btn">
+                    <button>Submit</button>
+                  </div>
                 </div>
-                  
+
                 <div className="result-weather">
-                  <h1 className="title"></h1>
-                  <p className="content"></p>
+                  <div className="result">
+                    <h1 className="title"></h1>
+                    <p className="content"></p>
+                  </div>
                 </div>
               </div>
             </div>
