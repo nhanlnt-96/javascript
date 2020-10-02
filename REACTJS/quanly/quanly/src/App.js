be581b7dead67a0c392329f39d09b0fb85b4702e
Lê Nguyễn Thiện Nhân <ThienNhan@MacBook-Air-cua-Le.local>
2020-10-02 14:00:18 +0700

    .

---
+++ REACTJS/quanly/quanly/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/img/unnamed.png';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="container">
+        <header>
+          
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
