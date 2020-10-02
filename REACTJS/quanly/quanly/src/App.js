7d585378d42174895fb188fbec01d4054ff202bb
Nhan <nhanlnt@hotmail.com>
2020-10-02 17:18:56 +0700

    nhan

--- REACTJS/quanly/quanly/src/App.js
+++ REACTJS/quanly/quanly/src/App.js
@@ -8,8 +8,34 @@ class App extends React.Component {
     return (
       <div className="container">
         <header>
-          
+          <div className="main-header">
+            <div className="toolbar">
+              <button onClick="">TRANG CHỦ</button>
+            </div>
+            
+            <div className="toolbar">
+              <button onClick="">HƯỚNG DẪN</button>
+            </div>
+            
+            <div className="logo">
+              <img src={logo} />
+            </div>
+            
+            <div className="toolbar">
+              <button onClick="">THÊM NHÂN VIÊN MỚI</button>
+            </div>
+            
+            <div className="toolbar">
+              <button onClick="">QUẢN LÝ NHÂN VIÊN</button>
+            </div>
+          </div>
         </header>
+
+        <div className="main-body">
+          <div className="slider">
+            
+          </div>
+        </div>
       </div>
     )
   }
