bc8add27dc9b867959777bfa64e95fa60d4980ef
Nhan <nhanlnt@hotmail.com>
2020-11-06 17:05:07 +0700

    addFeatures

--- Project/weather/src/App.js
+++ Project/weather/src/App.js
@@ -22,6 +22,10 @@ class App extends React.Component {
       theTemp1: '',
       maxTemp1: '',
       minTemp1: '',
+      windSpeed1: '',
+      humidity1: '',
+      visibility1: '',
+      pressure1: '',
       weatherStateAbbr1: '',
 
       applicableDate2: '',
@@ -29,6 +33,10 @@ class App extends React.Component {
       theTemp2: '',
       maxTemp2: '',
       minTemp2: '',
+      windSpeed2: '',
+      humidity2: '',
+      visibility2: '',
+      pressure2: '',
       weatherStateAbbr2: '',
 
       applicableDate3: '',
@@ -36,6 +44,10 @@ class App extends React.Component {
       theTemp3: '',
       maxTemp3: '',
       minTemp3: '',
+      windSpeed3: '',
+      humidity3: '',
+      visibility3: '',
+      pressure3: '',
       weatherStateAbbr3: '',
 
       applicableDate4: '',
@@ -43,6 +55,10 @@ class App extends React.Component {
       theTemp4: '',
       maxTemp4: '',
       minTemp4: '',
+      windSpeed4: '',
+      humidity4: '',
+      visibility4: '',
+      pressure4: '',
       weatherStateAbbr4: '',
 
       applicableDate5: '',
@@ -50,6 +66,10 @@ class App extends React.Component {
       theTemp5: '',
       maxTemp5: '',
       minTemp5: '',
+      windSpeed5: '',
+      humidity5: '',
+      visibility5: '',
+      pressure5: '',
       weatherStateAbbr5: '',
 
       applicableDate6: '',
@@ -57,6 +77,10 @@ class App extends React.Component {
       theTemp6: '',
       maxTemp6: '',
       minTemp6: '',
+      windSpeed6: '',
+      humidity6: '',
+      visibility6: '',
+      pressure6: '',
       weatherStateAbbr6: '',
     }
   }
@@ -74,6 +98,10 @@ class App extends React.Component {
           theTemp1: <p>{Math.round(result.consolidated_weather[0].the_temp)}&deg;</p>,
           maxTemp1: <p>Max: {Math.round(result.consolidated_weather[0].max_temp)}&deg;</p>,
           minTemp1: <p>Min: {Math.round(result.consolidated_weather[0].min_temp)}&deg;</p>,
+          windSpeed1: `Gió ${result.consolidated_weather[0].wind_direction_compass} ${Math.round(result.consolidated_weather[0].wind_speed)}mph`,
+          humidity1: `Độ ẩm ${result.consolidated_weather[0].humidity}%`,
+          visibility1: `Tầm nhìn xa ${(result.consolidated_weather[0].visibility).toFixed(1)} miles`,
+          pressure1: `Áp suất ${Math.round(result.consolidated_weather[0].air_pressure)}mb`,
           weatherStateAbbr1: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[0].weather_state_abbr}.svg`,
 
           applicableDate2: moment(result.consolidated_weather[1].applicable_date).format('DD-MM-YYYY'),
@@ -81,6 +109,10 @@ class App extends React.Component {
           theTemp2: <p>{Math.round(result.consolidated_weather[1].the_temp)}&deg;</p>,
           maxTemp2: <p>Max: {Math.round(result.consolidated_weather[1].max_temp)}&deg;</p>,
           minTemp2: <p>Min: {Math.round(result.consolidated_weather[1].min_temp)}&deg;</p>,
+          windSpeed2: `Gió ${result.consolidated_weather[1].wind_direction_compass} ${Math.round(result.consolidated_weather[1].wind_speed)}mph`,
+          humidity2: `Độ ẩm ${result.consolidated_weather[1].humidity}%`,
+          visibility2: `Tầm nhìn xa ${(result.consolidated_weather[1].visibility).toFixed(1)} miles`,
+          pressure2: `Áp suất ${Math.round(result.consolidated_weather[1].air_pressure)}mb`,
           weatherStateAbbr2: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[1].weather_state_abbr}.svg`,
 
           applicableDate3: moment(result.consolidated_weather[2].applicable_date).format('DD-MM-YYYY'),
@@ -88,6 +120,11 @@ class App extends React.Component {
           theTemp3: <p>{Math.round(result.consolidated_weather[2].the_temp)}&deg;</p>,
           maxTemp3: <p>Max: {Math.round(result.consolidated_weather[2].max_temp)}&deg;</p>,
           minTemp3: <p>Min: {Math.round(result.consolidated_weather[2].min_temp)}&deg;</p>,
+          windCompass3: ``,
+          windSpeed3: `Gió ${result.consolidated_weather[2].wind_direction_compass} ${Math.round(result.consolidated_weather[2].wind_speed)}mph`,
+          humidity3: `Độ ẩm ${result.consolidated_weather[2].humidity}%`,
+          visibility3: `Tầm nhìn xa ${(result.consolidated_weather[2].visibility).toFixed(1)} miles`,
+          pressure3: `Áp suất ${Math.round(result.consolidated_weather[2].air_pressure)}mb`,
           weatherStateAbbr3: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[2].weather_state_abbr}.svg`,
 
           applicableDate4: moment(result.consolidated_weather[3].applicable_date).format('DD-MM-YYYY'),
@@ -95,6 +132,10 @@ class App extends React.Component {
           theTemp4: <p>{Math.round(result.consolidated_weather[3].the_temp)}&deg;</p>,
           maxTemp4: <p>Max: {Math.round(result.consolidated_weather[3].max_temp)}&deg;</p>,
           minTemp4: <p>Min: {Math.round(result.consolidated_weather[3].min_temp)}&deg;</p>,
+          windSpeed4: `Gió ${result.consolidated_weather[3].wind_direction_compass} ${Math.round(result.consolidated_weather[3].wind_speed)}mph`,
+          humidity4: `Độ ẩm ${result.consolidated_weather[3].humidity}%`,
+          visibility4: `Tầm nhìn xa ${(result.consolidated_weather[3].visibility).toFixed(1)} miles`,
+          pressure4: `Áp suất ${Math.round(result.consolidated_weather[3].air_pressure)}mb`,
           weatherStateAbbr4: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[3].weather_state_abbr}.svg`,
 
           applicableDate5: moment(result.consolidated_weather[4].applicable_date).format('DD-MM-YYYY'),
@@ -102,6 +143,10 @@ class App extends React.Component {
           theTemp5: <p>{Math.round(result.consolidated_weather[4].the_temp)}&deg;</p>,
           maxTemp5: <p>Max: {Math.round(result.consolidated_weather[4].max_temp)}&deg;</p>,
           minTemp5: <p>Min: {Math.round(result.consolidated_weather[4].min_temp)}&deg;</p>,
+          windSpeed5: `Gió ${result.consolidated_weather[4].wind_direction_compass} ${Math.round(result.consolidated_weather[4].wind_speed)}mph`,
+          humidity5: `Độ ẩm ${result.consolidated_weather[4].humidity}%`,
+          visibility5: `Tầm nhìn xa ${(result.consolidated_weather[4].visibility).toFixed(1)} miles`,
+          pressure5: `Áp suất ${Math.round(result.consolidated_weather[4].air_pressure)}mb`,
           weatherStateAbbr5: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[4].weather_state_abbr}.svg`,
 
           applicableDate6: moment(result.consolidated_weather[5].applicable_date).format('DD-MM-YYYY'),
@@ -109,6 +154,10 @@ class App extends React.Component {
           theTemp6: <p>{Math.round(result.consolidated_weather[5].the_temp)}&deg;</p>,
           maxTemp6: <p>Max: {Math.round(result.consolidated_weather[5].max_temp)}&deg;</p>,
           minTemp6: <p>Min: {Math.round(result.consolidated_weather[5].min_temp)}&deg;</p>,
+          windSpeed6: `Gió ${result.consolidated_weather[5].wind_direction_compass} ${Math.round(result.consolidated_weather[5].wind_speed)}mph`,
+          humidity6: `Độ ẩm ${result.consolidated_weather[5].humidity}%`,
+          visibility6: `Tầm nhìn xa ${(result.consolidated_weather[5].visibility).toFixed(1)} miles`,
+          pressure6: `Áp suất ${Math.round(result.consolidated_weather[5].air_pressure)}mb`,
           weatherStateAbbr6: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[5].weather_state_abbr}.svg`,
         });
       },
@@ -160,47 +209,72 @@ class App extends React.Component {
           weatherResult: result.data,
           cityShowTitle: result.data.title,
 
-          applicableDate1: moment(result.data.consolidated_weather[0].applicable_date).format('DD-MM-YYYY'),
-          weatherStateName1: result.data.consolidated_weather[0].weather_state_name,
-          theTemp1: <p>{Math.round(result.data.consolidated_weather[0].the_temp)}&deg;</p>,
-          maxTemp1: <p>Max: {Math.round(result.data.consolidated_weather[0].max_temp)}&deg;</p>,
-          minTemp1: <p>Min: {Math.round(result.data.consolidated_weather[0].min_temp)}&deg;</p>,
-          weatherStateAbbr1: `https://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[0].weather_state_abbr}.svg`,
+          applicableDate1: moment(result.consolidated_weather[0].applicable_date).format('DD-MM-YYYY'),
+          weatherStateName1: result.consolidated_weather[0].weather_state_name,
+          theTemp1: <p>{Math.round(result.consolidated_weather[0].the_temp)}&deg;</p>,
+          maxTemp1: <p>Max: {Math.round(result.consolidated_weather[0].max_temp)}&deg;</p>,
+          minTemp1: <p>Min: {Math.round(result.consolidated_weather[0].min_temp)}&deg;</p>,
+          windSpeed1: `Gió ${result.consolidated_weather[0].wind_direction_compass} ${Math.round(result.consolidated_weather[0].wind_speed)}mph`,
+          humidity1: `Độ ẩm ${result.consolidated_weather[0].humidity}%`,
+          visibility1: `Tầm nhìn xa ${(result.consolidated_weather[0].visibility).toFixed(1)} miles`,
+          pressure1: `Áp suất ${Math.round(result.consolidated_weather[0].air_pressure)}mb`,
+          weatherStateAbbr1: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[0].weather_state_abbr}.svg`,
 
-          applicableDate2: moment(result.data.consolidated_weather[1].applicable_date).format('DD-MM-YYYY'),
-          weatherStateName2: result.data.consolidated_weather[1].weather_state_name,
-          theTemp2: <p>{Math.round(result.data.consolidated_weather[1].the_temp)}&deg;</p>,
-          maxTemp2: <p>Max: {Math.round(result.data.consolidated_weather[1].max_temp)}&deg;</p>,
-          minTemp2: <p>Min: {Math.round(result.data.consolidated_weather[1].min_temp)}&deg;</p>,
-          weatherStateAbbr2: `https://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[1].weather_state_abbr}.svg`,
+          applicableDate2: moment(result.consolidated_weather[1].applicable_date).format('DD-MM-YYYY'),
+          weatherStateName2: result.consolidated_weather[1].weather_state_name,
+          theTemp2: <p>{Math.round(result.consolidated_weather[1].the_temp)}&deg;</p>,
+          maxTemp2: <p>Max: {Math.round(result.consolidated_weather[1].max_temp)}&deg;</p>,
+          minTemp2: <p>Min: {Math.round(result.consolidated_weather[1].min_temp)}&deg;</p>,
+          windSpeed2: `Gió ${result.consolidated_weather[1].wind_direction_compass} ${Math.round(result.consolidated_weather[1].wind_speed)}mph`,
+          humidity2: `Độ ẩm ${result.consolidated_weather[1].humidity}%`,
+          visibility2: `Tầm nhìn xa ${(result.consolidated_weather[1].visibility).toFixed(1)} miles`,
+          pressure2: `Áp suất ${Math.round(result.consolidated_weather[1].air_pressure)}mb`,
+          weatherStateAbbr2: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[1].weather_state_abbr}.svg`,
 
-          applicableDate3: moment(result.data.consolidated_weather[2].applicable_date).format('DD-MM-YYYY'),
-          weatherStateName3: result.data.consolidated_weather[2].weather_state_name,
-          theTemp3: <p>{Math.round(result.data.consolidated_weather[2].the_temp)}&deg;</p>,
-          maxTemp3: <p>Max: {Math.round(result.data.consolidated_weather[2].max_temp)}&deg;</p>,
-          minTemp3: <p>Min: {Math.round(result.data.consolidated_weather[2].min_temp)}&deg;</p>,
-          weatherStateAbbr3: `https://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[2].weather_state_abbr}.svg`,
+          applicableDate3: moment(result.consolidated_weather[2].applicable_date).format('DD-MM-YYYY'),
+          weatherStateName3: result.consolidated_weather[2].weather_state_name,
+          theTemp3: <p>{Math.round(result.consolidated_weather[2].the_temp)}&deg;</p>,
+          maxTemp3: <p>Max: {Math.round(result.consolidated_weather[2].max_temp)}&deg;</p>,
+          minTemp3: <p>Min: {Math.round(result.consolidated_weather[2].min_temp)}&deg;</p>,
+          windCompass3: ``,
+          windSpeed3: `Gió ${result.consolidated_weather[2].wind_direction_compass} ${Math.round(result.consolidated_weather[2].wind_speed)}mph`,
+          humidity3: `Độ ẩm ${result.consolidated_weather[2].humidity}%`,
+          visibility3: `Tầm nhìn xa ${(result.consolidated_weather[2].visibility).toFixed(1)} miles`,
+          pressure3: `Áp suất ${Math.round(result.consolidated_weather[2].air_pressure)}mb`,
+          weatherStateAbbr3: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[2].weather_state_abbr}.svg`,
 
-          applicableDate4: moment(result.data.consolidated_weather[3].applicable_date).format('DD-MM-YYYY'),
-          weatherStateName4: result.data.consolidated_weather[3].weather_state_name,
-          theTemp4: <p>{Math.round(result.data.consolidated_weather[3].the_temp)}&deg;</p>,
-          maxTemp4: <p>Max: {Math.round(result.data.consolidated_weather[3].max_temp)}&deg;</p>,
-          minTemp4: <p>Min: {Math.round(result.data.consolidated_weather[3].min_temp)}&deg;</p>,
-          weatherStateAbbr4: `https://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[3].weather_state_abbr}.svg`,
+          applicableDate4: moment(result.consolidated_weather[3].applicable_date).format('DD-MM-YYYY'),
+          weatherStateName4: result.consolidated_weather[3].weather_state_name,
+          theTemp4: <p>{Math.round(result.consolidated_weather[3].the_temp)}&deg;</p>,
+          maxTemp4: <p>Max: {Math.round(result.consolidated_weather[3].max_temp)}&deg;</p>,
+          minTemp4: <p>Min: {Math.round(result.consolidated_weather[3].min_temp)}&deg;</p>,
+          windSpeed4: `Gió ${result.consolidated_weather[3].wind_direction_compass} ${Math.round(result.consolidated_weather[3].wind_speed)}mph`,
+          humidity4: `Độ ẩm ${result.consolidated_weather[3].humidity}%`,
+          visibility4: `Tầm nhìn xa ${(result.consolidated_weather[3].visibility).toFixed(1)} miles`,
+          pressure4: `Áp suất ${Math.round(result.consolidated_weather[3].air_pressure)}mb`,
+          weatherStateAbbr4: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[3].weather_state_abbr}.svg`,
 
-          applicableDate5: moment(result.data.consolidated_weather[4].applicable_date).format('DD-MM-YYYY'),
-          weatherStateName5: result.data.consolidated_weather[4].weather_state_name,
-          theTemp5: <p>{Math.round(result.data.consolidated_weather[4].the_temp)}&deg;</p>,
-          maxTemp5: <p>Max: {Math.round(result.data.consolidated_weather[4].max_temp)}&deg;</p>,
-          minTemp5: <p>Min: {Math.round(result.data.consolidated_weather[4].min_temp)}&deg;</p>,
-          weatherStateAbbr5: `https://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[4].weather_state_abbr}.svg`,
+          applicableDate5: moment(result.consolidated_weather[4].applicable_date).format('DD-MM-YYYY'),
+          weatherStateName5: result.consolidated_weather[4].weather_state_name,
+          theTemp5: <p>{Math.round(result.consolidated_weather[4].the_temp)}&deg;</p>,
+          maxTemp5: <p>Max: {Math.round(result.consolidated_weather[4].max_temp)}&deg;</p>,
+          minTemp5: <p>Min: {Math.round(result.consolidated_weather[4].min_temp)}&deg;</p>,
+          windSpeed5: `Gió ${result.consolidated_weather[4].wind_direction_compass} ${Math.round(result.consolidated_weather[4].wind_speed)}mph`,
+          humidity5: `Độ ẩm ${result.consolidated_weather[4].humidity}%`,
+          visibility5: `Tầm nhìn xa ${(result.consolidated_weather[4].visibility).toFixed(1)} miles`,
+          pressure5: `Áp suất ${Math.round(result.consolidated_weather[4].air_pressure)}mb`,
+          weatherStateAbbr5: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[4].weather_state_abbr}.svg`,
 
-          applicableDate6: moment(result.data.consolidated_weather[5].applicable_date).format('DD-MM-YYYY'),
-          weatherStateName6: result.data.consolidated_weather[5].weather_state_name,
-          theTemp6: <p>{Math.round(result.data.consolidated_weather[5].the_temp)}&deg;</p>,
-          maxTemp6: <p>Max: {Math.round(result.data.consolidated_weather[5].max_temp)}&deg;</p>,
-          minTemp6: <p>Min: {Math.round(result.data.consolidated_weather[5].min_temp)}&deg;</p>,
-          weatherStateAbbr6: `https://www.metaweather.com/static/img/weather/${result.data.consolidated_weather[5].weather_state_abbr}.svg`,
+          applicableDate6: moment(result.consolidated_weather[5].applicable_date).format('DD-MM-YYYY'),
+          weatherStateName6: result.consolidated_weather[5].weather_state_name,
+          theTemp6: <p>{Math.round(result.consolidated_weather[5].the_temp)}&deg;</p>,
+          maxTemp6: <p>Max: {Math.round(result.consolidated_weather[5].max_temp)}&deg;</p>,
+          minTemp6: <p>Min: {Math.round(result.consolidated_weather[5].min_temp)}&deg;</p>,
+          windSpeed6: `Gió ${result.consolidated_weather[5].wind_direction_compass} ${Math.round(result.consolidated_weather[5].wind_speed)}mph`,
+          humidity6: `Độ ẩm ${result.consolidated_weather[5].humidity}%`,
+          visibility6: `Tầm nhìn xa ${(result.consolidated_weather[5].visibility).toFixed(1)} miles`,
+          pressure6: `Áp suất ${Math.round(result.consolidated_weather[5].air_pressure)}mb`,
+          weatherStateAbbr6: `https://www.metaweather.com/static/img/weather/${result.consolidated_weather[5].weather_state_abbr}.svg`,
         });
       } catch (error) {
         this.setState ({
@@ -261,14 +335,27 @@ class App extends React.Component {
               </div>
 
               <div className="weather-result-detail">
-                <div className="city-title">
-                  <p>{this.state.cityShowTitle}</p>
-                </div>
+                <div className="weather-main-day-result">
+                  <div className="weather-main-day-header">
+                    <div className="city-title">
+                      {this.state.cityShowTitle}
+                    </div>
 
-                <div className="weather-day-detail">
-                  <div className="weather-day-header">
-                    <div className="weather-day">
-                      {this.state.applicableDate1}
+                    <div className="main-day-min-max-temp">
+                      {this.state.minTemp1}
+                      {this.state.maxTemp1}
+                    </div>                  
+                  </div>
+
+                  <div className="weather-main-day-content">
+                    <div className="wind-speed">
+                      {this.state.windSpeed1}
+                      <br></br>
+                      {this.state.pressure1}
+                      <br></br>
+                      {this.state.humidity1}
+                      <br></br>
+                      {this.state.visibility1}
                     </div>
                   </div>
                 </div>
