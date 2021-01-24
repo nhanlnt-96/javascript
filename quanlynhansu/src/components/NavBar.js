f77f136f1f69bab212d090f25104900fa76eeeab
Lnt.Nhan <nhanlnt@hotmail.com>
2021-01-24 12:15:40 +0700

    done NavBar

--- quanlynhansu/src/components/NavBar.js
+++ quanlynhansu/src/components/NavBar.js
@@ -1,37 +1,42 @@
 import React from 'react';
 import logo from '../assets/img/logo.png';
-import { Menu, Dropdown, Button, Space } from 'antd';
-import '../App.css';
-import 'antd/dist/antd.css';
 
 export const NavBar = () => {
-    const menu = (
-        <Menu>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
-            </Menu.Item>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-                    3rd menu item
-      </a>
-            </Menu.Item>
-        </Menu>
-    )
     return (
         <div className="NavBar">
-            <div className="logo">
+            <div className="left-bar">
                 <img src={logo} alt="logo" />
-                <h1>HỆ THỐNG GIÁO DỤC TÂN VĂN</h1>
+                <div className="logo-title">
+                    <h1>HỆ THỐNG GIÁO DỤC</h1>
+                    <h1>TÂN VĂN</h1>
+                </div>
             </div>
 
-            <div className="navbar-menu">
-                <Space direction="vertical">
-                    <Space wrap>
-                        <Dropdown overplay={menu} placement="bottomCenter">
-                            <Button>Nhan</Button>
-                        </Dropdown>
-                    </Space>
-                </Space>
+            <div className="right-bar">
+                <button>TRANG CHỦ</button>
+
+                <div className="dropdown">
+                    <button>QUẢN LÝ NHÂN SỰ</button>
+                    <ul>
+                        <li>THÊM NHÂN VIÊN MỚI</li>
+                        <li>THÔNG TIN NHÂN VIÊN</li>
+                        <li>CHẤM CÔNG</li>
+                    </ul>
+                </div>
+
+                <div className="dropdown">
+                    <button>PHÂN CÔNG GIÁO VIÊN</button>
+                    <ul>
+                        <li>GIÁO VIÊN VIỆT NAM</li>
+                        <li>GIÁO VIÊN NƯỚC NGOÀI</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="burger-nav">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
             </div>
         </div>
     );
