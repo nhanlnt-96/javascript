82b4e5b013410873429c2791cefc09d66d287f82
Lnt.Nhan <nhanlnt@hotmail.com>
2021-02-01 22:02:04 +0700

    add redux folder

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -12,34 +12,32 @@ import { createStore } from 'redux';
 const increment = () => {
   return {
     type: 'INCREMENT'
-  };
-};
+  }
+}
 
 const decrement = () => {
   return {
     type: 'DECREMENT'
-  };
-};
+  }
+}
 
 //REDUCER
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1;
+      return state + 1
     case 'DECREMENT':
-      return state - 1;
-  };
-};
+      return state - 1
+  }
+}
 
 let store = createStore(counter);
 
-// Display it in the console
+//Display it in the console
 store.subscribe(() => console.log(store.getState()));
 
-//DISPATCH 
+//DISPATCH
 store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
 store.dispatch(decrement());
 */
 
