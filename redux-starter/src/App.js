f0f0ffa0168e48fd8c603b523b18c238be1e1f9d
Nhan <nhanlnt@hotmail.com>
2021-02-02 16:39:31 +0700

    done redux-starter

--- redux-starter/src/App.js
+++ redux-starter/src/App.js
@@ -1,10 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { increment, decrement } from './actions';
+
 import './App.css';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      
-    </div>
+    <div className="App" >
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      { isLogged ? <h3>Valuable information I shouldn't see</h3> : ''}
+    </div >
   );
 }
 
--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import allReducer from './reducers';
+
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 /*
 import { createStore } from 'redux';
 
@@ -43,7 +48,9 @@ store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
